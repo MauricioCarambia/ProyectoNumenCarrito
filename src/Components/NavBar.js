524fcd4f36788a35026aee6fcd77eea16a28bc15
Mauricio Carambia <mcarambia@gmail.com>
2023-04-03 01:01:05 -0300

    logo

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -2,6 +2,9 @@ import React from "react";
 
 const NavBar = () => {
     return (
+        <div>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxxvXarDEKq6q0KAVQNi_vq5K3fKG23aGTYA&usqp=CAU" alt="logo" width="100px"/>
+            <p>Empresa</p>
         <ul className="nav justify-content-end">
             <li className="nav-item">
                 <a className="nav-link active" aria-current="page" href="#">
@@ -23,6 +26,7 @@ const NavBar = () => {
                     Contacto</a>
             </li>
         </ul>
+        </div>
     );
 };
 
